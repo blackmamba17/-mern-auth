ca65b2c997090bfc436056a776e351efa91c4719
gtorrisi <scuolatorrisi@gmail.com>
2024-05-25 19:24:23 +0200

    middleware and custom error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
 
   //bcrypt and salt
@@ -13,6 +14,6 @@ export const signup = async (req, res) => {
 
     res.status(201).json({ message: "user created successfully" });
   } catch (error) {
-    res.status(500).json(error.message);
+    next(error);
   }
 };
--- api/index.js
+++ api/index.js
@@ -11,6 +11,9 @@ import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 
+//bodyparser
+import bodyParser from "body-parser";
+
 dotenv.config();
 
 //connect to the mongolodb
@@ -29,6 +32,7 @@ const app = express();
 
 //parser
 app.use(express.json());
+app.use(bodyParser.json());
 
 app.listen(3000, () => {
   console.log("server listening on port 3000");
@@ -37,3 +41,13 @@ app.listen(3000, () => {
 //api/user path and from this i go to forward / userroutes
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "internal server error";
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
+  });
+});
