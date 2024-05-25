084ceb055a955ebe4dd8e797d333c3689aaee21b
gtorrisi <scuolatorrisi@gmail.com>
2024-05-25 13:06:20 +0200

    added mongodb to backend

--- api/index.js
+++ api/index.js
@@ -1,6 +1,23 @@
 import { log } from "console";
 import express from "express";
 
+//database
+import mongoose from "mongoose";
+
+//env var
+import dotenv from "dotenv";
+dotenv.config();
+
+//connect to the mongolodb
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected to mongodb");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 //create backend
 
 const app = express();
